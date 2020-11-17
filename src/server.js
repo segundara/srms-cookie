06b0b8ab9c67296eb1d47bdca5e3fdca2e0158cf
segundara <segundara@gmail.com>
2020-11-17 18:41:53 +0200

    cors options

--- src/server.js
+++ src/server.js
@@ -1,62 +1,63 @@
-const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-dotenv.config()
-const db = require("./db")
-const studentRouter = require("./routes/students")
-const departmentRouter = require("./routes/departments")
-const courseRouter = require("./routes/courses")
-const lecturerRouter = require("./routes/lecturers")
-const registerRouter = require("./routes/course_register")
-const examRouter = require("./routes/exams")
-const adminRouter = require("./routes/admin")
-const userRouter = require("./routes/users")
-const listEndpoints = require("express-list-endpoints")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const db = require("./db");
+const studentRouter = require("./routes/students");
+const departmentRouter = require("./routes/departments");
+const courseRouter = require("./routes/courses");
+const lecturerRouter = require("./routes/lecturers");
+const registerRouter = require("./routes/course_register");
+const examRouter = require("./routes/exams");
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/users");
+const listEndpoints = require("express-list-endpoints");
+const cookieParser = require("cookie-parser");
 
 const {
-    notFoundHandler,
-    forbiddenHandler,
-    badRequestHandler,
-    genericErrorHandler,
-} = require("./errorHandlers")
+  notFoundHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./errorHandlers");
 
-const server = express()
+const server = express();
 
-// const whitelist = ["http://localhost:3000"]
-const whitelist = ["https://srms-ck-fe.herokuapp.com"]
+// const whitelist = ["http://localhost:3000"];
+const whitelist = ["https://srms-fe.herokuapp.com"]
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error("Not allowed by CORS"))
-        }
-    },
-    credentials: true,
-    methods: "GET, POST, PUT, DELETE, HEAD, OPTIONS"
-}
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: "GET, POST, PUT, DELETE, HEAD, OPTIONS",
+};
 
-server.use(cors(corsOptions))
-server.use(express.json())
-server.use(cookieParser())
+server.use(cors(corsOptions));
+server.use(express.json());
+server.use(cookieParser());
 
-
-server.use("/student", studentRouter)
-server.use("/departments", departmentRouter)
-server.use("/courses", courseRouter)
-server.use("/tutor", lecturerRouter)
-server.use("/register", registerRouter)
-server.use("/exams", examRouter)
-server.use("/admin", adminRouter)
-server.use("/users", userRouter)
+server.use("/student", studentRouter);
+server.use("/departments", departmentRouter);
+server.use("/courses", courseRouter);
+server.use("/tutor", lecturerRouter);
+server.use("/register", registerRouter);
+server.use("/exams", examRouter);
+server.use("/admin", adminRouter);
+server.use("/users", userRouter);
 
 // ERROR HANDLERS MIDDLEWARES
 
-server.use(badRequestHandler)
-server.use(forbiddenHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
+server.use(badRequestHandler);
+server.use(forbiddenHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
-console.log(listEndpoints(server))
-server.listen(process.env.PORT || 3456, () => console.log("Running on ", process.env.PORT || 3456))
+console.log(listEndpoints(server));
+server.listen(process.env.PORT || 3456, () =>
+  console.log("Running on ", process.env.PORT || 3456)
+);
