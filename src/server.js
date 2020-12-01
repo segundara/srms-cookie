d36a0b523d3a72002c83f8bc0e1ce5f459d9b55a
segundara <segundara@gmail.com>
2020-12-01 07:58:49 +0200

    checking another FE

--- src/server.js
+++ src/server.js
@@ -24,8 +24,8 @@ const {
 const server = express();
 
 // const whitelist = ["http://localhost:3000"];
-// const whitelist = ["https://srms-fe.herokuapp.com"]
-const whitelist = ["https://srms-ck-fe.herokuapp.com"]
+const whitelist = ["https://srms-fe.herokuapp.com"]
+// const whitelist = ["https://srms-ck-fe.herokuapp.com"]
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
